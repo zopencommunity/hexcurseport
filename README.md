3babacd962707cb472114c583edc34f6f41adcca
Igor Todorovski <itodorov@ca.ibm.com>
2025-02-09 17:48:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/hexcurseport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/hexcurseport/actions/workflows/bump.yml)
+
+# hexcurse
+
+Hexcurse is a ncurses-based console hexeditor written in C
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/hexcurseport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/hexcurseport.git'), string(name: 'PORT_DESCRIPTION', value: 'Hexcurse is a ncurses-based console hexeditor written in C' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
